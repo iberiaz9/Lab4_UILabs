f9fb70b409faa6e6f0c6e12f3195db281f50f461
Jose Alvarado <alvarado.ja53@gmail.com>
2016-09-26 13:41:31 -0700

    Initial commit

---
+++ app/src/main/java/course/labs/todomanager/ToDoItem.java
@@ -0,0 +1,150 @@
+package course.labs.todomanager;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.content.Intent;
+
+public class ToDoItem {
+
+	public static final String ITEM_SEP = System.getProperty("line.separator");
+
+	public enum Priority {
+		LOW, MED, HIGH
+	};
+
+	public enum Status {
+		NOTDONE, DONE
+	};
+
+	public final static String TITLE = "title";
+	public final static String PRIORITY = "priority";
+	public final static String STATUS = "status";
+	public final static String DATE = "date";
+
+	// Adding note to program 9/23/16
+	public final static String NOTE = "note";
+
+	public final static String FILENAME = "filename";
+
+	public final static SimpleDateFormat FORMAT = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm:ss", Locale.US);
+
+	private String mTitle = new String();
+
+	// Adding note to program 9/23/16
+	private String mNote = new String();
+
+	private Priority mPriority = Priority.LOW;
+	private Status mStatus = Status.NOTDONE;
+	private Date mDate = new Date();
+
+	ToDoItem(String title, Priority priority, Status status, Date date, String note) {
+		this.mTitle = title;
+		this.mPriority = priority;
+		this.mStatus = status;
+		this.mDate = date;
+		this.mNote = note;
+	}
+
+	// Create a new ToDoItem from data packaged in an Intent
+
+	ToDoItem(Intent intent) {
+
+		mTitle = intent.getStringExtra(ToDoItem.TITLE);
+		mPriority = Priority.valueOf(intent.getStringExtra(ToDoItem.PRIORITY));
+		mStatus = Status.valueOf(intent.getStringExtra(ToDoItem.STATUS));
+		// note added to program 9/23/16
+		mNote = intent.getStringExtra(ToDoItem.NOTE);
+
+		try {
+			mDate = ToDoItem.FORMAT.parse(intent.getStringExtra(ToDoItem.DATE));
+		} catch (ParseException e) {
+			mDate = new Date();
+		}
+	}
+
+	public String getTitle() {
+		return mTitle;
+	}
+
+	// Adding note to program 9/23/16
+	public String getNote() {
+		return mNote;
+	}
+
+    // Adding note to program 9/23/16
+    public void setNote(String note) {
+        mNote = note;
+    }
+
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	public Priority getPriority() {
+		return mPriority;
+	}
+
+	public void setPriority(Priority priority) {
+		mPriority = priority;
+	}
+
+	public Status getStatus() {
+		return mStatus;
+	}
+
+	public void setStatus(Status status) {
+		mStatus = status;
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	// Take a set of String data values and 
+	// package them for transport in an Intent
+
+	public static void packageIntent(Intent intent, String title,
+			Priority priority, Status status, String date, String note) {
+
+		intent.putExtra(ToDoItem.TITLE, title);
+		intent.putExtra(ToDoItem.PRIORITY, priority.toString());
+		intent.putExtra(ToDoItem.STATUS, status.toString());
+		intent.putExtra(ToDoItem.DATE, date);
+        // note added to program 9/23/16
+        intent.putExtra(ToDoItem.NOTE, note);
+	
+	}
+
+	public void packageIntent2(Intent intent) {
+
+		intent.putExtra(ToDoItem.TITLE, mTitle);
+		// note added to program 9/23/16
+		intent.putExtra(ToDoItem.NOTE, mNote);
+		intent.putExtra(ToDoItem.PRIORITY, mPriority.toString());
+		intent.putExtra(ToDoItem.STATUS, mStatus.toString());
+		intent.putExtra(ToDoItem.DATE, FORMAT.format(mDate));
+
+	}
+
+	public String toString() {
+		mNote = mNote.replaceAll("\n", " ");
+		
+		return mTitle + ITEM_SEP + mPriority + ITEM_SEP + mStatus + ITEM_SEP
+				+ FORMAT.format(mDate) + ITEM_SEP + mNote;
+	}
+
+	public String toLog() {
+		return "Title:" + mTitle + ITEM_SEP + "Priority:" + mPriority
+				+ ITEM_SEP + "Status:" + mStatus + ITEM_SEP + "Date:"
+				+ FORMAT.format(mDate) + "\n";
+	}
+
+}
---
+++ app/src/main/java/course/labs/todomanager/ToDoListAdapter.java
@@ -0,0 +1,188 @@
+package course.labs.todomanager;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class ToDoListAdapter extends BaseAdapter {
+
+	private final List<ToDoItem> mItems = new ArrayList<ToDoItem>();
+	private final Context mContext;
+	private ToDoManagerActivity mToDoManagerActivity;
+	private static final String TAG = "Lab-UserInterface";
+	// Date for overdue checkbox added 9/25/16
+	private Date mDate;
+
+	public ToDoListAdapter(Context context, ToDoManagerActivity toDoManagerActivity) {
+
+		mToDoManagerActivity = toDoManagerActivity;
+		mContext = context;
+
+	}
+
+	// Add a ToDoItem to the adapter
+	// Notify observers that the data set has changed
+
+	public void add(ToDoItem item) {
+
+		mItems.add(item);
+		notifyDataSetChanged();
+
+	}
+
+	public void remove(ToDoItem item) {
+
+		mItems.remove(item);
+		notifyDataSetChanged();
+
+	}
+
+	// Clears the list adapter of all items.
+
+	public void clear() {
+
+		mItems.clear();
+		notifyDataSetChanged();
+
+	}
+
+	// Returns the number of ToDoItems
+
+	@Override
+	public int getCount() {
+
+		return mItems.size();
+
+	}
+
+	// Retrieve the number of ToDoItems
+
+	@Override
+	public Object getItem(int pos) {
+
+		return mItems.get(pos);
+
+	}
+
+	// Get the ID for the ToDoItem
+	// In this case it's just the position
+
+	@Override
+	public long getItemId(int pos) {
+
+		return pos;
+
+	}
+
+	// Create a View for the ToDoItem at specified position
+	// Remember to check whether convertView holds an already allocated View
+	// before created a new View.
+	// Consider using the ViewHolder pattern to make scrolling more efficient
+	// See: http://developer.android.com/training/improving-layouts/smooth-scrolling.html
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		// TODO - Get the current ToDoItem
+		//final ToDoItem toDoItem = null;
+		final ToDoItem toDoItem = (ToDoItem) getItem(position);
+
+		// TODO - Inflate the View for this ToDoItem
+		// from todo_item.xml
+		//RelativeLayout itemLayout = null;
+		RelativeLayout itemLayout = (RelativeLayout) LayoutInflater.from (mContext)
+				.inflate(R.layout.todo_item, null);
+
+		// Fill in specific ToDoItem data
+		// Remember that the data that goes in this View
+		// corresponds to the user interface elements defined
+		// in the layout file
+
+		// TODO - Display Title in TextView
+		//final TextView titleView = null;
+		final  TextView titleView = (TextView) itemLayout.findViewById(R.id.titleView);
+		titleView.setText(toDoItem.getTitle());
+
+
+		// TODO - Set up Status CheckBox
+		// Set up overdue check box
+		final CheckBox overDueChkBox = (CheckBox) itemLayout.findViewById(R.id.overDueCheckBox);
+
+
+
+		//final CheckBox statusView = null;
+		final CheckBox statusView = (CheckBox) itemLayout.findViewById(R.id.statusCheckBox);
+		statusView.setChecked(ToDoItem.Status.DONE.equals(toDoItem.getStatus()));
+
+		mDate = new Date();
+		if (toDoItem.getDate().before(mDate) && !statusView.isChecked()) {
+			overDueChkBox.setChecked(true);
+		}
+
+		// TODO - Must also set up an OnCheckedChangeListener,
+		// which is called when the user toggles the status checkbox
+
+		statusView.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+					@Override
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+
+                        if (statusView.isChecked()) {
+                            toDoItem.setStatus(ToDoItem.Status.DONE);
+                            overDueChkBox.setChecked(false);
+                        }
+                        else if (!statusView.isChecked()) {
+                            toDoItem.setStatus(ToDoItem.Status.NOTDONE);
+                            if (toDoItem.getDate().before(mDate)) {
+                                overDueChkBox.setChecked(true);
+                            }
+                        }
+					}
+				});
+
+		itemLayout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				//TODO - Implement OnClick().
+				mToDoManagerActivity.updateTodoItem(toDoItem);
+			}
+		});
+
+		// TODO - Display Priority in a TextView
+		//final TextView priorityView = null;
+		final TextView priorityView = (TextView) itemLayout.findViewById(R.id.priorityView);
+		priorityView.setText(toDoItem.getPriority().toString());
+
+
+		// TODO - Display Time and Date.
+		// Hint - use ToDoItem.FORMAT.format(toDoItem.getDate()) to get date and
+		// time String
+		//final TextView dateView = null;
+		final TextView dateView = (TextView) itemLayout.findViewById(R.id.dateView);
+		dateView.setText(ToDoItem.FORMAT.format(toDoItem.getDate()));
+
+		// Adding note to list adapter 9/23/16
+
+		final TextView noteView = (TextView) itemLayout.findViewById(R.id.noteView);
+		noteView.setText(toDoItem.getNote());
+
+
+		// Return the View you just created
+		return itemLayout;
+
+	}
+}
